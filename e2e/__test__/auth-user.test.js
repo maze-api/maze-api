1d5fb475136f675f408bc559e27dceca78720f82
Dave Trost <davetrost@gmail.com>
2019-10-14 12:31:13 -0700

    roles functionality

--- e2e/__test__/auth-user.test.js
+++ e2e/__test__/auth-user.test.js
@@ -4,39 +4,32 @@ const { signupUser } = require('../data-helpers');
 const User = require('../../lib/models/user');
 
 describe('Auth-User API', () => {
-  
   const testUser = {
     email: 'me@me.com',
     password: 'abc'
   };
-  
+
   const testAdmin = {
     email: 'admin@admin.com',
     password: 'abc'
   };
-  
+
   beforeEach(() => dropCollection('users'));
   beforeEach(() => dropCollection('keys'));
-  
-  let testUserId;
 
+  let testAdminKey;
   beforeEach(() => {
-    return signupUser(testAdmin)
-      .then(user => {        
-        return User.updateById(user._id, {
-          $addToSet: {
-            roles: 'admin'
-          }
-        });
-      })
-      .then(() => {
-        return request
-          .post('/api/auth/signin')
-          .send(testAdmin)
-          .expect(200);
+    return signupUser(testAdmin).then(user => {
+      testAdminKey = user.key;
+      return User.updateById(user._id, {
+        $addToSet: {
+          roles: 'admin'
+        }
       });
+    });
   });
 
+  let testUserId;
   beforeEach(() => {
     return signupUser(testUser).then(user => {
       testUserId = user._id;
@@ -46,20 +39,22 @@ describe('Auth-User API', () => {
   it('puts a role into user', () => {
     return request
       .put(`/api/auth/users/${testUserId}/roles/bottom-dweller`)
+      .set('Authorization', testAdminKey)
       .expect(200)
       .then(({ body }) => {
         expect(body).toMatchInlineSnapshot(
           {
             _id: expect.any(String),
-            hash: expect.any(String)
+            hash: expect.any(String),
+            key: expect.any(String)
           },
           `
           Object {
             "__v": 0,
             "_id": Any<String>,
             "email": "me@me.com",
-            "favorites": Array [],
             "hash": Any<String>,
+            "key": Any<String>,
             "roles": Array [
               "bottom-dweller",
             ],
@@ -72,24 +67,27 @@ describe('Auth-User API', () => {
   it('deletes a user role', () => {
     return request
       .put(`/api/auth/users/${testUserId}/roles/bottom-dweller`)
+      .set('Authorization', testAdminKey)
       .expect(200)
       .then(() => {
         return request
           .delete(`/api/auth/users/${testUserId}/roles/bottom-dweller`)
+          .set('Authorization', testAdminKey)
           .expect(200)
           .then(({ body }) => {
             expect(body).toMatchInlineSnapshot(
               {
                 _id: expect.any(String),
-                hash: expect.any(String)
+                hash: expect.any(String),
+                key: expect.any(String)
               },
               `
               Object {
                 "__v": 0,
                 "_id": Any<String>,
                 "email": "me@me.com",
-                "favorites": Array [],
                 "hash": Any<String>,
+                "key": Any<String>,
                 "roles": Array [],
               }
             `
@@ -98,20 +96,62 @@ describe('Auth-User API', () => {
       });
   });
 
-  it('gets all users and returns email and roles', () => {
+  it('gets all users with roles and with API key details', () => {
     return request
       .get('/api/auth/users')
+      .set('Authorization', testAdminKey)
       .expect(200)
       .then(({ body }) => {
         expect(body.length).toBe(2);
         expect(body[0]).toMatchInlineSnapshot(
           {
-            _id: expect.any(String)
+            _id: expect.any(String),
+            hash: expect.any(String),
+            key: {
+              _id: expect.any(String),
+              created: expect.any(String)
+            }
           },
           `
           Object {
+            "__v": 0,
             "_id": Any<String>,
             "email": "admin@admin.com",
+            "hash": Any<String>,
+            "key": Object {
+              "__v": 0,
+              "_id": Any<String>,
+              "active": true,
+              "created": Any<String>,
+            },
+            "roles": Array [
+              "admin",
+            ],
+          }
+        `
+        );
+      });
+  });
+
+  it('deletes a user from the database', () => {
+    return request
+      .delete(`/api/auth/users/${testUserId}`)
+      .set('Authorization', testAdminKey)
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toMatchInlineSnapshot(
+          {
+            _id: expect.any(String),
+            hash: expect.any(String),
+            key: expect.any(String)
+          },
+          `
+          Object {
+            "__v": 0,
+            "_id": Any<String>,
+            "email": "admin@admin.com",
+            "hash": Any<String>,
+            "key": Any<String>,
             "roles": Array [
               "admin",
             ],

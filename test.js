63997b55a27ef82e6168edd71c5c5b3e7a7aab57
Dave Trost <davetrost@gmail.com>
2019-10-14 11:31:36 -0700

    solution path functional

--- test.js
+++ test.js
@@ -4,14 +4,19 @@ let maze;
 maze = new HexMaze(8, 8);
 maze.calculateDifficultiesFromStartingPoint(1, 1);
 const longestMaxXEdgeCellSolution = maze.cells[maze.maxX].reduce((distObj, cell) => {
-  const newObj = { distance: cell.distanceFromStart, coord: `(${cell.x}, ${cell.y})` };
+  const newObj = { distance: cell.distanceFromStart, x: cell.x, y: cell.y };
   return (newObj.distance > distObj.distance) ? newObj : distObj;
-}, { distance: 0, coord: '0, 0' });
+}, { distance: 0, x: -1, y: -1 });
 console.log('starting coords: (1, 1)');
-console.log('longest right-edge solution distance:', longestMaxXEdgeCellSolution.distance, longestMaxXEdgeCellSolution.coord);
+console.log(`longest right-edge solution distance: ${longestMaxXEdgeCellSolution.distance} at (${longestMaxXEdgeCellSolution.x}, ${longestMaxXEdgeCellSolution.y})`);
 console.log('leaf cell count:', maze.numLeafCells);
 console.log('average distance of non-solution trails:', (maze.numCells - longestMaxXEdgeCellSolution.distance) / maze.numLeafCells);
+console.log('average distance of all trails:', maze.numCells / maze.numLeafCells);
+
 console.log(maze.printCells());
+
+console.log(maze.solutionPathFromEndCoords(longestMaxXEdgeCellSolution.x, longestMaxXEdgeCellSolution.y));
+
 // maze.traverseCells(cell => console.log(cell.x, cell.y, ': ', cell.distanceFromStart));
 
 // maze = new HexMaze(8, 8);

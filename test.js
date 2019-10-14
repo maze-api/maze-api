b553c3511abaf62e5b8467bf30a83f71ff224c46
Dave Trost <davetrost@gmail.com>
2019-10-14 13:59:09 -0700

    export to maze model

--- test.js
+++ test.js
@@ -1,23 +1,25 @@
 const { HexMaze } = require('./lib/algorithms/hexagonalRecursiveBacktracker/Maze');
 let maze;
 
-maze = new HexMaze(8, 8);
-maze.calculateDifficultiesFromStartingPoint(1, 1);
-const longestMaxXEdgeCellSolution = maze.cells[maze.maxX].reduce((distObj, cell) => {
-  const newObj = { distance: cell.distanceFromStart, x: cell.x, y: cell.y };
-  return (newObj.distance > distObj.distance) ? newObj : distObj;
-}, { distance: 0, x: -1, y: -1 });
-console.log('starting coords: (1, 1)');
-console.log(`longest right-edge solution distance: ${longestMaxXEdgeCellSolution.distance} at (${longestMaxXEdgeCellSolution.x}, ${longestMaxXEdgeCellSolution.y})`);
-console.log('leaf cell count:', maze.numLeafCells);
-console.log('average distance of non-solution trails:', (maze.numCells - longestMaxXEdgeCellSolution.distance) / maze.numLeafCells);
-console.log('average distance of all trails:', maze.numCells / maze.numLeafCells);
+// maze = new HexMaze(30, 20);
+// maze.calculateDifficultiesFromStartingPoint(1, 1);
+// const longestMaxXEdgeCellSolution = maze.cells[maze.maxX].reduce((distObj, cell) => {
+//   const newObj = { distance: cell.distanceFromStart, x: cell.x, y: cell.y };
+//   return (newObj.distance > distObj.distance) ? newObj : distObj;
+// }, { distance: 0, x: -1, y: -1 });
+// console.log('starting coords: (1, 1)');
+// console.log(`longest right-edge solution distance: ${longestMaxXEdgeCellSolution.distance} at (${longestMaxXEdgeCellSolution.x}, ${longestMaxXEdgeCellSolution.y})`);
+// console.log('leaf cell count:', maze.numLeafCells);
+// console.log('average distance of non-solution trails:', (maze.numCells - longestMaxXEdgeCellSolution.distance) / maze.numLeafCells);
+// console.log('average distance of all trails:', maze.numCells / maze.numLeafCells);
 
+// console.log(maze.printCells());
+
+// console.log(maze.solutionPathFromEndCoords(longestMaxXEdgeCellSolution.x, longestMaxXEdgeCellSolution.y));
+
+maze = new HexMaze(3, 3);
 console.log(maze.printCells());
-
-console.log(maze.solutionPathFromEndCoords(longestMaxXEdgeCellSolution.x, longestMaxXEdgeCellSolution.y));
-
-// maze.traverseCells(cell => console.log(cell.x, cell.y, ': ', cell.distanceFromStart));
+console.log(maze.cells);
 
 // maze = new HexMaze(8, 8);
 // console.log(maze.printCells());

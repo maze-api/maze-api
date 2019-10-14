d5b1d8a7a40ebc52839b72f8d79a3c5ff926f24b
Joe Klause <joeklause@gmail.com>
2019-10-14 10:55:12 -0700

    user routes finished

--- e2e/__test__/auth.test.js
+++ e2e/__test__/auth.test.js
@@ -1,41 +1,29 @@
 const request = require('../request');
 const { dropCollection } = require('../db');
 const { signupUser } = require('../data-helpers');
+const User = require('../../lib/models/user');
+const Key = require('../../lib/models/key-model');
 
 describe('Auth API', () => {
+
+
+
+  beforeEach(() => dropCollection('users'));
+  beforeEach(() => dropCollection('keys'));
 
   const testUser = {
     email: 'me@me.com',
     password: 'abc'
   };
 
-  beforeEach(() => dropCollection('users'));
-
-  let api_key;
+  let apiKey;
   beforeEach(() => {
     return signupUser()
       .then(user => {
-        api_key = user.key;
+        apiKey = user.key;
       });
   });
 
-  
-  it('signs up a user', () => {
-    expect(api_key).toBeDefined;
-  });
-
-
-  function testBadSignin(testName, user) {
-    it(testName, () => {
-      return request
-        .post('/api/auth/signin')
-        .send(user)
-        .expect(401)
-        .then(({ body }) => {
-          expect(body.error).toBe('Invalid email or password');
-        });
-    });
-  }
 
 
   testBadSignin('rejects bad password', {
@@ -46,6 +34,12 @@ describe('Auth API', () => {
   testBadSignin('rejects invalid email', {
     email: 'bad@email.com',
     password: testUser.password
+  });
+
+
+    
+  it('signs up a user', () => {
+    expect(apiKey).toBeDefined;
   });
 
 
@@ -73,6 +67,130 @@ describe('Auth API', () => {
       });
   });
 
+  it('updates key', () => {
+    return request
+      .put('/api/auth/refresh-key')
+      .send(testUser)
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toBeDefined();
+        expect(body).not.toBe(apiKey);
+      });
+  });
+
+  it('doesn\'t allow users without proper email to update key', () => {
+    return request
+      .put('/api/auth/refresh-key')
+      .send({
+        email: 'bad@email.com',
+        password: testUser.password
+      })
+      .expect(401)
+      .then(({ body }) => {
+        expect(body.error).toBe('Invalid email or password');
+      });
+  });
+
+  it('doesn\'t allow users without proper email to update key', () => {
+    return request
+      .put('/api/auth/refresh-key')
+      .send({
+        email: 'me@me.com',
+        password: 'bad'
+      })
+      .expect(401)
+      .then(({ body }) => {
+        expect(body.error).toBe('Invalid email or password');
+      });
+  });
+
+  it('it allows remover to delete themselves', () => {
+    return request
+      .delete('/api/auth')
+      .send(testUser)
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.email).toBe(testUser.email);
+        expect(body.key._id).toBe(apiKey);
+        return body;
+      })
+
+      .then(user => {
+        User.findById(user._id)
+          .then(user => {
+            expect(user).toBe(null);
+          });
+      });
+  });
+
+  it('removes user key as well as user', () => {
+    return request
+      .delete('/api/auth')
+      .send(testUser)
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.email).toBe(testUser.email);
+        expect(body.key._id).toBe(apiKey);
+        return body;
+      })
+
+      .then(user => {
+        Key.findById(user.key._id)
+          .then(key => {
+            expect(key).toBe(null);
+          });
+      });
+  });
+
+  it('doesn\'t allow users without proper email to delete themselves', () => {
+    return request
+      .delete('/api/auth')
+      .send({
+        email: 'bad@email.com',
+        password: testUser.password
+      })
+      .expect(401)
+      .then(({ body }) => {
+        expect(body.error).toBe('Invalid email or password');
+      });
+  });
+
+  it('doesn\'t allow users without proper email to delete themselves', () => {
+    return request
+      .delete('/api/auth')
+      .send({
+        email: 'me@me.com',
+        password: 'bad'
+      })
+      .expect(401)
+      .then(({ body }) => {
+        expect(body.error).toBe('Invalid email or password');
+      });
+  });
+
+
+  testEmailAndPasswordRequired('signup', 'email', { password: 'I no like emails' });
+  testEmailAndPasswordRequired('signup', 'password', { email: 'no@password.com' });
+
+  testEmailAndPasswordRequired('signin', 'email', { password: 'I no like emails' });
+  testEmailAndPasswordRequired('signin', 'password', { email: 'no@password.com' });
+
+
+
+
+  function testBadSignin(testName, user) {
+    it(testName, () => {
+      return request
+        .post('/api/auth/signin')
+        .send(user)
+        .expect(401)
+        .then(({ body }) => {
+          expect(body.error).toBe('Invalid email or password');
+        });
+    });
+  }
+
+
   function testEmailAndPasswordRequired(route, testProperty, user) {
     it(`${route} requires ${testProperty}`, () => {
       return request
@@ -84,12 +202,6 @@ describe('Auth API', () => {
         });
     });
   }
-
-  testEmailAndPasswordRequired('signup', 'email', { password: 'I no like emails' });
-  testEmailAndPasswordRequired('signup', 'password', { email: 'no@password.com' });
-
-  testEmailAndPasswordRequired('signin', 'email', { password: 'I no like emails' });
-  testEmailAndPasswordRequired('signin', 'password', { email: 'no@password.com' });
 
 
 });

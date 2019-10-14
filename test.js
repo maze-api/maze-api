390fc7b380c74363440f3ac1c1d448cbece3b2c9
Dave Trost <davetrost@gmail.com>
2019-10-14 00:19:18 -0700

    distance from start functionality

--- test.js
+++ test.js
@@ -1,16 +1,21 @@
 const { HexMaze } = require('./lib/algorithms/hexagonalRecursiveBacktracker/Maze');
 let maze;
 
-// maze = new HexMaze(3, 3);
+maze = new HexMaze(5, 5);
+maze.calculateDifficultiesFromStartingPoint(1, 5);
+
+console.log(maze.printCells());
+console.log('leaf cell count:', maze.numLeafCells);
+console.log('starting coords: (1, 5) \n');
+maze.traverseCells(cell => console.log(cell.x, cell.y, ': ', cell.distanceFromStart));
+
+// maze = new HexMaze(8, 8);
+// console.log(maze.printCells());
+// maze = new HexMaze(9, 9);
 // console.log(maze.printCells());
 
-maze = new HexMaze(8, 8);
-console.log(maze.printCells());
-maze = new HexMaze(9, 9);
-console.log(maze.printCells());
+// maze = new HexMaze(20, 20);
+// console.log(maze.printCells());
 
-maze = new HexMaze(20, 20);
-console.log(maze.printCells());
-
-maze = new HexMaze(40, 40);
-console.log(maze.printCells());
+// maze = new HexMaze(40, 40);
+// console.log(maze.printCells());

f652082fef4d120594f6abb36ce0ee57ac2a6a19
Dave Trost <davetrost@gmail.com>
2019-10-14 00:51:05 -0700

    difficulty testing

--- test.js
+++ test.js
@@ -1,13 +1,18 @@
 const { HexMaze } = require('./lib/algorithms/hexagonalRecursiveBacktracker/Maze');
 let maze;
 
-maze = new HexMaze(5, 5);
-maze.calculateDifficultiesFromStartingPoint(1, 5);
-
-console.log(maze.printCells());
+maze = new HexMaze(8, 8);
+maze.calculateDifficultiesFromStartingPoint(1, 1);
+const longestMaxXEdgeCellSolution = maze.cells[maze.maxX].reduce((distObj, cell) => {
+  const newObj = { distance: cell.distanceFromStart, coord: `(${cell.x}, ${cell.y})` };
+  return (newObj.distance > distObj.distance) ? newObj : distObj;
+}, { distance: 0, coord: '0, 0' });
+console.log('starting coords: (1, 1)');
+console.log('longest right-edge solution distance:', longestMaxXEdgeCellSolution.distance, longestMaxXEdgeCellSolution.coord);
 console.log('leaf cell count:', maze.numLeafCells);
-console.log('starting coords: (1, 5) \n');
-maze.traverseCells(cell => console.log(cell.x, cell.y, ': ', cell.distanceFromStart));
+console.log('average distance of non-solution trails:', (maze.numCells - longestMaxXEdgeCellSolution.distance) / maze.numLeafCells);
+console.log(maze.printCells());
+// maze.traverseCells(cell => console.log(cell.x, cell.y, ': ', cell.distanceFromStart));
 
 // maze = new HexMaze(8, 8);
 // console.log(maze.printCells());

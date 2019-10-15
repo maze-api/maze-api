3163b6cf434acc15003c943b6b214dea99391689
Jose <j.a.ojeda@live.com>
2019-10-15 10:20:42 -0700

    deleted unused lines in user, started testing connect.js

---
+++ e2e/__test__/connect.test.js
@@ -0,0 +1,18 @@
+// const connect = require('../../lib/connect');
+
+// describe('connect', () => {
+//   const options = {
+//     log: true
+//   };
+
+//   const testURI = 'testURI';
+
+//   it('???', () => {
+//     return Promise.resolve(console.log(connect(testURI, options)))
+//       .catch(err => {
+//         console.log(err);
+      
+//       });
+    
+//   });
+// });

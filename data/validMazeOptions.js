7cd73a68070057752ab61d0909e320b41c32a211
Dave Trost <davetrost@gmail.com>
2019-10-14 22:38:33 -0700

    maze e2e test updates in progress

---
+++ data/validMazeOptions.js
@@ -0,0 +1,12 @@
+
+const validHexOptions = {
+  topologyName: 'Hexagonal',
+  algorithm: 'Recursive Back Tracker',
+  dimensions: { height: 3, width: 3 },
+  difficulty: 'Easier',
+  start: { x: 1, y: 1 },
+};
+
+module.exports = {
+  validHexOptions,
+}; 

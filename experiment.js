432b5679e13b7db871a7947852d75ca7c5e899d1
Dave Trost <davetrost@gmail.com>
2019-10-16 15:57:15 -0700

    experiment updates

--- experiment.js
+++ experiment.js
@@ -1,26 +1,37 @@
 const { HexesMaze } = require('./lib/maze-classes/HexesMaze');
 const { SquaresMaze } = require('./lib/maze-classes/SquaresMaze');
+let options;
 
-const options = {
+
+options = {
   width: 10,
   height: 10,
   startX: 1,
   startY: 1,
-  endX: 5,
-  endY: 7,
 };
-
-let maze;
-
-// options.algorithm = 'Recursive Backtracker';
-options.algorithm = 'Woven';
-maze = new SquaresMaze(options);
-console.log(maze.exportMazeModel().averagePathLength);
-console.log(maze.exportMazeModel().averageWrongPathLength);
-console.log(maze.exportMazeModel().solutionLength);
-console.log(maze.printCells());
-
 options.algorithm = 'Recursive Backtracker';
-maze = new HexesMaze(options);
-//console.log(maze.printCells());
+options.algorithm = 'Woven';
+// options.endX = 5;
+// options.endY = 7;
+const squaresMaze = new SquaresMaze(options);
+// console.log(squaresMaze.exportMazeModel().averageWrongPathLength);
+// console.log(squaresMaze.exportMazeModel().averagePathLength);
+// console.log(squaresMaze.exportMazeModel().solutionLength);
+console.log(squaresMaze.printCells());
+
+
+options = {
+  width: 11,
+  height: 10,
+  startX: 1,
+  startY: 1,
+};
+options.algorithm = 'Recursive Backtracker';
+// options.endX = 10;
+// options.endY = 2;
+const hexesMaze = new HexesMaze(options);
+// console.log(hexesMaze.exportMazeModel().averagePathLength);
+// console.log(hexesMaze.exportMazeModel().averageWrongPathLength);
+// console.log(hexesMaze.exportMazeModel().solutionLength);
+console.log(hexesMaze.printCells());
 

faa4a0c5adea950f59247266f5e5442b95c6264b
Dave Trost <davetrost@gmail.com>
2019-10-17 12:38:40 -0700

    test & throw for specific invalid maze options

--- data/mazeOptions.js
+++ data/mazeOptions.js
@@ -5,19 +5,45 @@ const validHexOptions = {
   dimensions: { height: 3, width: 3 },
   start: { x: 1, y: 1 },
 };
+
 const validSquareOptions = {
   cellShape: 'Square',
   algorithm: 'Recursive Backtracker',
   dimensions: { height: 3, width: 3 },
   start: { x: 1, y: 1 },
 };
+
+const tooSmallDimensionsOptions = {
+  cellShape: 'Square',
+  algorithm: 'Recursive Backtracker',
+  dimensions: { height: 1, width: 1 },
+  start: { x: 1, y: 1 },
+  end: { x: 1, y: 1 }
+};
+
+const invalidStartPointOptions = {
+  cellShape: 'Square',
+  algorithm: 'Recursive Backtracker',
+  dimensions: { height: 3, width: 3 },
+  start: { x: 4, y: 11 },
+  end: { x: 1, y: 3 }
+};
+
+const duplicateStartAndEndOptions = {
+  cellShape: 'Square',
+  algorithm: 'Recursive Backtracker',
+  dimensions: { height: 3, width: 3 },
+  start: { x: 2, y: 2 },
+  end: { x: 2, y: 2 }
+};
+
 const invalidEndCoordOptions = {
   cellShape: 'Square',
   algorithm: 'Recursive Backtracker',
   dimensions: { height: 3, width: 3 },
   start: { x: 1, y: 1 },
   end: { x: 1 }
-};
+};  
 
 const invalidEndPointOptions = {
   cellShape: 'Square',
@@ -25,7 +51,15 @@ const invalidEndPointOptions = {
   dimensions: { height: 3, width: 3 },
   start: { x: 1, y: 1 },
   end: { x: 1, y: 9 }
-};
+};  
+
+const incompatibleAlgoAndCellShapeOptions = {
+  cellShape: 'Hexagonal',
+  algorithm: 'Woven',
+  dimensions: { height: 3, width: 3 },
+  start: { x: 1, y: 1 },
+  end: { x: 1, y: 1 }
+};  
 
 const castErrorOptions = {
   cellShape: 'Square',
@@ -37,7 +71,11 @@ const castErrorOptions = {
 module.exports = {
   validHexOptions,
   validSquareOptions,
+  tooSmallDimensionsOptions,
+  invalidStartPointOptions,
+  duplicateStartAndEndOptions,
   invalidEndCoordOptions,
   invalidEndPointOptions,
+  incompatibleAlgoAndCellShapeOptions,
   castErrorOptions
 }; 

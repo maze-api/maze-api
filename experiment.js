f3b785d0d096fd20c7cb4e92cc7622aca1ab6de6
Dave Trost <davetrost@gmail.com>
2019-10-17 13:34:24 -0700

    changes to experiment file

--- experiment.js
+++ experiment.js
@@ -1,16 +1,19 @@
-// const { HexesMaze } = require('./lib/maze-classes/HexesMaze');
-const { SquaresMaze } = require('./lib/maze-classes/SquaresMaze');
+const { HexesMaze } = require('./lib/maze-classes/HexesMaze');
+// const { SquaresMaze } = require('./lib/maze-classes/SquaresMaze');
 
 
 let options1 = {
-  width: 21,
-  height: 21,
+  width: 10,
+  height: 10,
   startX: 1,
   startY: 1,
-  algorithm: 'Woven'
 };
-let maze1 = new SquaresMaze(options1);
-console.log(maze1.exportMazeModel());
+options1.algorithm = 'Recursive Backtracker';
+// options1.algorithm = 'Growing Tree';
+// options1.algorithm = 'Prims';
+// options1.algorithm = 'Woven';
+// let maze1 = new SquaresMaze(options1);
+let maze1 = new HexesMaze(options1);
 console.log(maze1.printCells());
 console.log(maze1.printCells(false));
 
